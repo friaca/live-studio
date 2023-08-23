ea511be8eb9f077a5676caafad6a81152e8e6eff
friaca <friacajoao@gmail.com>
2023-08-23 00:09:45 -0300

    feat: badge component on CustomComponents

--- lib/live_view_studio_web/components/custom_components.ex
+++ lib/live_view_studio_web/components/custom_components.ex
@@ -1,11 +1,15 @@
 defmodule LiveViewStudioWeb.CustomComponents do
   use Phoenix.Component
 
+  embed_templates "icons/*"
+
   attr :expiration, :integer, default: 24
   slot :legal
   slot :inner_block, required: true
 
   def promo(assigns) do
+    assigns = assign_new(assigns, :minutes, fn -> assigns.expiration * 60 end)
+
     ~H"""
     <div class="promo">
       <div class="deal">
@@ -19,7 +23,22 @@ defmodule LiveViewStudioWeb.CustomComponents do
       <div class="legal">
         <%= render_slot(@legal) %>
       </div>
+       <%= @minutes %>
     </div>
+    """
+  end
+
+  attr :label, :string, required: true
+  attr :class, :string, default: nil
+  attr :rest, :global
+
+  def badge(assigns) do
+    ~H"""
+    <span class={["inline-flex items-center gap-0.5 rounded-full
+             bg-gray-300 px-3 py-0.5 text-sm font-medium
+             text-gray-800 hover:cursor-pointer", @class]} {@rest}>
+      <%= @label %> <Heroicons.x_mark class="h-3 w-3 text-gray-600" />
+    </span>
     """
   end
 end
