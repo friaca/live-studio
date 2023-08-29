4930285fcd160ea43c0f78d652700ea5fd66ce85
friaca <friacajoao@gmail.com>
2023-08-28 21:03:15 -0300

    feat(exercise): form for creating servers in ServersLive

--- lib/live_view_studio_web/live/servers_live.ex
+++ lib/live_view_studio_web/live/servers_live.ex
@@ -2,14 +2,17 @@ defmodule LiveViewStudioWeb.ServersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Servers
+  alias LiveViewStudio.Servers.Server
 
   def mount(_params, _session, socket) do
     servers = Servers.list_servers()
+    changeset = Servers.change_server(%Server{})
 
     socket =
       assign(socket,
         servers: servers,
-        coffees: 0
+        coffees: 0,
+        form: to_form(changeset)
       )
 
     {:ok, socket}
@@ -48,6 +51,7 @@ defmodule LiveViewStudioWeb.ServersLive do
       </div>
       <div class="main">
         <div class="wrapper">
+          <.server_form form={@form}></.server_form>
           <.server server={@selected_server} />
           <div class="links">
             <.link navigate={~p"/light"}>
@@ -57,6 +61,29 @@ defmodule LiveViewStudioWeb.ServersLive do
         </div>
       </div>
     </div>
+    """
+  end
+
+  def server_form(assigns) do
+    ~H"""
+    <.form for={@form} phx-submit="save">
+      <div class="field">
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
+      </div>
+      <div class="field">
+        <.input
+          field={@form[:framework]}
+          placeholder="Framework"
+          autocomplete="off"
+        />
+      </div>
+      <div class="field">
+        <.input field={@form[:size]} placeholder="Size (MB)" type="number" />
+      </div>
+      <.button phx-disable-with="Saving...">
+        Save
+      </.button>
+    </.form>
     """
   end
 
@@ -92,5 +119,24 @@ defmodule LiveViewStudioWeb.ServersLive do
 
   def handle_event("drink", _, socket) do
     {:noreply, update(socket, :coffees, &(&1 + 1))}
+  end
+
+  def handle_event("save", %{"server" => server_params}, socket) do
+    case Servers.create_server(server_params) do
+      {:ok, server} ->
+        changeset = Servers.change_server(%Server{})
+
+        socket =
+          socket
+          |> assign(:form, to_form(changeset))
+          |> update(:servers, fn servers -> [server | servers] end)
+
+        {:noreply, socket}
+
+      {:error, changeset} ->
+        socket = socket |> assign(:form, to_form(changeset))
+
+        {:noreply, socket}
+    end
   end
 end
